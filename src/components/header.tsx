cb1ed6900b7548fd12d02e8ee30241eed646aec2
banyudu <banyudu@gmail.com>
2022-09-04 01:52:47 +0800

    feat: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,26 @@
-import {
-  XOutlined,
-  MinusOutlined,
-  ArrowsExpandOutlined
-} from '@emmda-design/icons'
-import useTauriWindow from 'hooks/useTauriWindow'
+import { QuestionMarkCircleOutlined, UserCircleOutlined } from '@emmda-design/icons'
+import useTranslation from 'hooks/useTranslation'
 
 export default function Header () {
-  const iconCls =
-    'w-4 h-4 rounded-full flex items-center justify-center space-x-2 group'
-  const buttonCls = 'w-3 h-3 hidden group-hover:block'
-
-  const { tauriWindow } = useTauriWindow()
+  const { t } = useTranslation()
   return (
     <div
       className='w-full h-full flex items-center justify-between px-2'
-      data-tauri-drag-region
     >
-      <div className='controls flex w-14 justify-between'>
-        <div
-          className={`${iconCls} bg-orange-600`}
-          onClick={() => tauriWindow?.close()}
-        >
-          <XOutlined className={buttonCls} />
+      <div className='history-toolbar w-5/12 max-w-xs'></div>
+      <div className='flex flex-1 justify-between items-center h-full'>
+        <div className='search-bar h-full py-2'>
+          <input type="text" className='h-full px-2 w-64 focus:w-96 text-black' placeholder={t('search', 'Search')} />
         </div>
-        <div
-          className={`${iconCls} bg-yellow-600`}
-          onClick={() => tauriWindow?.minimize()}
-        >
-          <MinusOutlined className={buttonCls} />
-        </div>
-        <div
-          className={`${iconCls} bg-green-600`}
-          onClick={() => tauriWindow?.toggleMaximize()}
-        >
-          <ArrowsExpandOutlined className={buttonCls} />
+        <div className='actions h-full flex items-center justify-between w-12'>
+          <div className='help w-5'>
+            <QuestionMarkCircleOutlined />
+          </div>
+          <div className='user w-5'>
+            <UserCircleOutlined />
+          </div>
         </div>
       </div>
-      <div className='menu'>menu</div>
     </div>
   )
 }
